aac15193680ee7548ee7f1d678858832f0a8ef99
Anemro <97135868+Anemro@users.noreply.github.com>
2022-02-25 23:52:22 -0300

    Corrección de ciclos y calculos

--- Desafio1/desafio1.js
+++ Desafio1/desafio1.js
@@ -1,22 +1,53 @@
+//let capitalInicial = parseInt(prompt('Ingrese capital inicial'));
+//let interes = parseInt(prompt('Ingrese el interes % mensual a recibir'))
+//let composicion = parseInt(prompt('Ingrese la cantidad de meses que recibirá intereses'))
+//let anual = parseInt(prompt('Ingrese los años que durará la inversión'))
+//let capitalFinal = result(capitalInicial,compuesto(calcInteres(interes),composicion,anual));
 console.log("desafio");
 //Definimos las constantes de calculos
-const result = (a,b) => a * b
-const calcInteres = x => 1+(x*0.01)
-const compuesto = (a,b,c) => a**(b*c)//a es el interes acumulativo b es la periodicidad de los intereses y c es el ciclo de esos intereses periodicos.
+const producto = (a,b) => a * b //donde a es el capital inicial e b el resultado de los interes
+const basePotencia = (r)=> 1 + (r/100) //donde r es el interes mensual
+const calcInteres =(b,e)=> Math.pow(b,e)//donde b es la base y e el exponente resultante del producto frecuencia y anual
+
 //Definimos las variables necesarias para los calculos
-
-let capitalInicial = parseInt(prompt('Ingrese capital inicial'));
-let interes = parseInt(prompt('Ingrese el interes % mensual a recibir'))
-let composicion = parseInt(prompt('Ingrese la cantidad de meses que recibirá intereses'))
-let anual = parseInt(prompt('Ingrese los años que durará la inversión'))
-let capitalFinal = result(capitalInicial,compuesto(calcInteres(interes),composicion,anual));
-
-if (capitalInicial = capitalFinal) {
-    alert (capitalFinal)
-    console.log(capitalFinal)
-}
-else{
-    console.log('Algunos de los valores ingresados no es un nro')
+let capitalInicial= ''
+//Aportación
+let aportacion= ''
+let frecuenciaAporte=''
+//Interés devengado
+let interes=''
+let frecuencia=''
+let anual=''
+let capitalFinal=''
+while (true) {
+    
+    capitalInicial = parseInt(prompt('ingrese capital inicial'));
+    if (isNaN(capitalInicial)) {
+        alert('este valor no es valido');
+        break;
+    }
+    interes = parseInt(prompt('Ingrese interes mensual'));
+    if (isNaN(interes)) {
+        alert('este valor no es valido');
+        break;
+    }
+    frecuencia = parseInt(prompt('ingrese cantidad de meses que recibira interes'));
+    if (isNaN(anual)) {
+        alert('este valor no es valido');
+        break;
+    }
+    
+    anual = parseInt(prompt('ingrese cantidad de años que dura la inversión'));
+    if (isNaN(anual)) {
+        alert('este valor no es valido');
+        break;
+    }
+    else {
+        capitalFinal = producto(capitalInicial,calcInteres(basePotencia(interes),producto(frecuencia,anual)));
+        console.log(capitalFinal);
+        alert(capitalFinal);
+        break
+    }
 }
 
 
